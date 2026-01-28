0c38d3f476bc4a246d372a7db16faa3c8e9f34e6
Ishan-31-10 <ishan31fz@gmail.com>
2026-01-29 01:01:26 +0530

    Updated code for deployement

--- app/contact/page.js
+++ app/contact/page.js
@@ -176,13 +176,7 @@ export default function ContactPage() {
                       Our Office
                     </h3>
                     <p className="text-slate-600 leading-relaxed text-lg">
-                      NEST Dubai
-                      <br />
-                      TRYP by Wyndham
-                      <br />
-                      Al Thanyah First - Barsha Heights
-                      <br />
-                      Dubai, United Arab Emirates
+                      Al Thanyah First - Barsha Heights - Dubai - United Arab Emirates
                     </p>
                   </div>
                 </div>
@@ -238,21 +232,21 @@ export default function ContactPage() {
 
       {/* Map Section */}
       <section className="py-20 bg-white/50">
-        <div className="container mx-auto px-6">
-          <div className="rounded-3xl overflow-hidden shadow-2xl border-4 border-white">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.1953487127614!2d55.18469831501207!3d25.097419083944686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6b3f3f3f3f3f%3A0x3f3f3f3f3f3f3f3f!2sBarsha%20Heights%20-%20Dubai!5e0!3m2!1sen!2sae!4v1234567890123!5m2!1sen!2sae"
-              width="100%"
-              height="500"
-              style={{ border: 0 }}
-              allowFullScreen=""
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              title="Essence Mobi Office Location"
-            ></iframe>
-          </div>
-        </div>
-      </section>
+  <div className="container mx-auto px-6">
+    <div className="rounded-3xl overflow-hidden shadow-2xl border-4 border-white">
+      <iframe
+        src="https://www.google.com/maps?q=Barsha+Heights+Dubai&output=embed"
+        width="100%"
+        height="500"
+        style={{ border: 0 }}
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Essence Mobi Office Location"
+      ></iframe>
+    </div>
+  </div>
+</section>
+
     </div>
   );
 }
--- components/Footer.js
+++ components/Footer.js
@@ -74,7 +74,8 @@ export default function Footer() {
 
             <div className="rounded-2xl overflow-hidden border border-white/10 shadow-lg">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.1953487127614!2d55.18469831501207!3d25.097419083944686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6b3f3f3f3f3f%3A0x3f3f3f3f3f3f3f3f!2sBarsha%20Heights%20-%20Dubai!5e0!3m2!1sen!2sae!4v1234567890123!5m2!1sen!2sae"
+                // src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.1953487127614!2d55.18469831501207!3d25.097419083944686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6b3f3f3f3f3f%3A0x3f3f3f3f3f3f3f3f!2sBarsha%20Heights%20-%20Dubai!5e0!3m2!1sen!2sae!4v1234567890123!5m2!1sen!2sae"
+                src="https://www.google.com/maps?q=Barsha+Heights+Dubai&output=embed"
                 width="100%"
                 height="200"
                 style={{ border: 0 }}
